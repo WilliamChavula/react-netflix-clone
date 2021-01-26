c6bdbc3432beafda4c4ee14015f3d65ac67a6dda
WilliamChavula <rumbani@gmail.com>
2021-01-26 14:59:57 -0600

    created a selectionFilter() utility to filter films and series collections by genre and return new objects

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
+    // Get series and films from firebase
     const { series } = useContent('series');
-    console.log(series);
+    const { films } = useContent('films');
+
+    // get genre slides from films and series
+    const slides = selectionFilter({ series, films });
+    console.log(slides);
 
     return <div>Browse</div>;
 }
